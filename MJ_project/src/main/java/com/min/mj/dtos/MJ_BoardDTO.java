4c1c054dad7d3ac30d6339fd2114f5616620e01b
uriim <uriim@DESKTOP-PB02B5F>
2020-06-08 13:43:16 +0900

    20200608 작성자 : 김유림 내용 : 게터세터

--- MJ_project/src/main/java/com/min/mj/dtos/MJ_BoardDTO.java
+++ MJ_project/src/main/java/com/min/mj/dtos/MJ_BoardDTO.java
@@ -50,6 +50,112 @@ public class MJ_BoardDTO {
 		this.moneypub = moneypub;
 		this.realfile = realfile;
 	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getNicname() {
+		return nicname;
+	}
+
+	public void setNicname(String nicname) {
+		this.nicname = nicname;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getReadcount() {
+		return readcount;
+	}
+
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getMoneypub() {
+		return moneypub;
+	}
+
+	public void setMoneypub(String moneypub) {
+		this.moneypub = moneypub;
+	}
+
+	public String getImsi() {
+		return imsi;
+	}
+
+	public void setImsi(String imsi) {
+		this.imsi = imsi;
+	}
+
+	public String getJjim() {
+		return jjim;
+	}
+
+	public void setJjim(String jjim) {
+		this.jjim = jjim;
+	}
+
+	public String getDelflag() {
+		return delflag;
+	}
+
+	public void setDelflag(String delflag) {
+		this.delflag = delflag;
+	}
+
+	public String getRealfile() {
+		return realfile;
+	}
+
+	public void setRealfile(String realfile) {
+		this.realfile = realfile;
+	}
+
+	@Override
+	public String toString() {
+		return "MJ_BoardDTO [id=" + id + ", seq=" + seq + ", nicname=" + nicname + ", title=" + title + ", content="
+				+ content + ", readcount=" + readcount + ", regdate=" + regdate + ", moneypub=" + moneypub + ", imsi="
+				+ imsi + ", jjim=" + jjim + ", delflag=" + delflag + ", realfile=" + realfile + "]";
+	}
+	
+	
+	
 	
 	
 }
