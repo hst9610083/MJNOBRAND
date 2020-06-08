4ed272e627618f06ab5be907ece0a9e0de593e13
uriim <uriim@DESKTOP-PB02B5F>
2020-06-08 15:40:54 +0900

    0608 작성자 : 김유림 내용 : 게터세터 투스트링

--- MJ_project/src/main/java/com/min/mj/dtos/MJ_MemberDTO.java
+++ MJ_project/src/main/java/com/min/mj/dtos/MJ_MemberDTO.java
@@ -65,6 +65,152 @@ public class MJ_MemberDTO implements Serializable{
 		this.phonnum = phonnum;
 	}
 
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getSelpick() {
+		return selpick;
+	}
+
+	public void setSelpick(String selpick) {
+		this.selpick = selpick;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getNicname() {
+		return nicname;
+	}
+
+	public void setNicname(String nicname) {
+		this.nicname = nicname;
+	}
+
+	public String getSellernum() {
+		return sellernum;
+	}
+
+	public void setSellernum(String sellernum) {
+		this.sellernum = sellernum;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getPhonnum() {
+		return phonnum;
+	}
+
+	public void setPhonnum(String phonnum) {
+		this.phonnum = phonnum;
+	}
+
+	public String getRealfile() {
+		return realfile;
+	}
+
+	public void setRealfile(String realfile) {
+		this.realfile = realfile;
+	}
+
+	public int getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(int regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImsi() {
+		return imsi;
+	}
+
+	public void setImsi(String imsi) {
+		this.imsi = imsi;
+	}
+
+	@Override
+	public String toString() {
+		return "MJ_MemberDTO [seq=" + seq + ", selpick=" + selpick + ", id=" + id + ", pw=" + pw + ", name=" + name
+				+ ", address=" + address + ", nicname=" + nicname + ", sellernum=" + sellernum + ", email=" + email
+				+ ", auth=" + auth + ", birth=" + birth + ", phonnum=" + phonnum + ", realfile=" + realfile
+				+ ", regdate=" + regdate + ", title=" + title + ", content=" + content + ", imsi=" + imsi + "]";
+	}
+	
+	
+
 
 
 	
