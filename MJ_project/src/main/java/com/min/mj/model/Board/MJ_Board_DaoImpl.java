2feb487a9c5c8ca5c4805b225a5b8497dc2e29e2
hst9610083 <hst9610083@gmail.com>
2020-06-08 16:21:54 +0900

    2020 .06 .08 작성자 : 황성태 내용 : 쿼리 및 Dao, Service 작업 완료

---
+++ MJ_project/src/main/java/com/min/mj/model/Board/MJ_Board_DaoImpl.java
@@ -0,0 +1,65 @@
+package com.min.mj.model.Board;
+
+import java.util.List;
+import java.util.Map;
+
+import com.min.mj.dtos.MJ_BoardDTO;
+import com.min.mj.dtos.RowNumDto;
+
+public class MJ_Board_DaoImpl implements IMj_Board_Dao {
+
+	@Override
+	public List<MJ_BoardDTO> pplSelectBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MJ_BoardDTO pplgetOnBoard(String seq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean pplWriteBoard(MJ_BoardDTO dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pplReadcountBoard(String seq) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pplModifyBoard(MJ_BoardDTO dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pplDelBoard(String seq) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pplMultiDel(Map<String, String[]> map) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<MJ_BoardDTO> BoardListRow(RowNumDto dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int BoardListTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
