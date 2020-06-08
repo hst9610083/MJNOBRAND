33266224697aee1262f0dcc3764df11bd8af1784
uriim <uriim@DESKTOP-PB02B5F>
2020-06-08 16:50:56 +0900

    0608 작성자 : 김유림 내용 : DAO IDAO 만듬

--- MJ_project/src/main/java/com/min/mj/model/member/Mj_Member_DaoImpl.java
+++ MJ_project/src/main/java/com/min/mj/model/member/Mj_Member_DaoImpl.java
@@ -1,5 +1,46 @@
 package com.min.mj.model.member;
 
+
+import com.min.mj.dtos.MJ_MemberDTO;
+
+
 public class Mj_Member_DaoImpl implements IMj_Member_Dao {
+	
+	
+
+	@Override
+	public MJ_MemberDTO userlogin() {
+		return null;
+	}
+
+	@Override
+	public boolean s_register() {
+		return false;
+	}
+
+	@Override
+	public boolean c_register() {
+		return false;
+	}
+
+	@Override
+	public MJ_MemberDTO s_info() {
+		return null;
+	}
+
+	@Override
+	public boolean s_getout() {
+		return false;
+	}
+
+	@Override
+	public MJ_MemberDTO c_info() {
+		return null;
+	}
+
+	@Override
+	public boolean c_getout() {
+		return false;
+	}
 
 }
