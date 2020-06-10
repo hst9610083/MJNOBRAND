ba558c0f5ea664c975c5a0d4cd19519a5ae7d3ee
uriim <uriim@DESKTOP-PB02B5F>
2020-06-10 14:50:52 +0900

    0610 작성자 : 김유림 내용 : Exsi.xml 이랑 인쿼리.xml 수정

--- MJ_project/src/main/java/com/min/mj/ctrl/EstiController.java
+++ MJ_project/src/main/java/com/min/mj/ctrl/EstiController.java
@@ -1,10 +1,17 @@
 package com.min.mj.ctrl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.min.mj.dtos.EstiDto;
+import com.min.mj.dtos.INQUIRYBoardDto;
 import com.min.mj.model.Esti.IMj_Esti_Service;
 
 @Controller
@@ -14,7 +21,27 @@ public class EstiController {
 	@Autowired
 	private IMj_Esti_Service service;
 	
+	
+	//견적서 게시판 전체 리스트(소비자)
+	@RequestMapping(value="/c_EstiList.do",method = RequestMethod.GET)
+	public String c_EstiList(Model model,EstiDto dto) {
+		
+		List<EstiDto> lists=service.Cesti_All();
+		model.addAttribute("lists",lists);
+		return "c_EstiList";
+		
+	};
+	
 	//견적서 게시판 전체 리스트(업체)
+	@RequestMapping(value="/s_EstiList.do",method = RequestMethod.GET)
+	public String s_EstiList(Model model,EstiDto dto) {
+		
+		List<EstiDto> lists=service.Sesti_All();
+		model.addAttribute("lists",lists);
+		return "s_EstiList";
+		
+	};
+	
 	
 	
 }
