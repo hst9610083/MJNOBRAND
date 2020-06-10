3c378d71008bfde6318e733b07a28280e6ffc7d0
Administrator <Administrator@DESKTOP-VJROMU1>
2020-06-10 10:19:25 +0900

    2020.06.10 작성자 : 정우민 내용 : 오라클설정

--- MJ_project/src/main/java/com/min/mj/ctrl/InquiryController.java
+++ MJ_project/src/main/java/com/min/mj/ctrl/InquiryController.java
@@ -1,10 +1,16 @@
 package com.min.mj.ctrl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.min.mj.dtos.INQUIRYBoardDto;
 import com.min.mj.model.Inquiry.IMj_Inquiry_Service;
 
 @Controller
@@ -14,7 +20,13 @@ public class InquiryController {
 	@Autowired
 	private IMj_Inquiry_Service service;
 	
-	//문의글 게시판 전체리스트
-	
-	
+	//임시 문의글 게시판 전체리스트
+	@RequestMapping(value="/iBoardList.do",method = RequestMethod.GET)
+	public String iBoardList(Model model,INQUIRYBoardDto dto) {
+		
+		List<INQUIRYBoardDto> lists=service.Mimsi_All();
+		model.addAttribute("lists",lists);
+		return "iBoardList";
+		
+	};
 }
