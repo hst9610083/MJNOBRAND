466a32391f20833528a999f193e5928e95744f14
ChoYoungRae <cho02116@naver.com>
2020-06-15 15:29:11 +0900

    2020.06.15 작성자 조영래 내용 재커밋

--- MJ_project/src/main/java/com/min/mj/ctrl/MemberController.java
+++ MJ_project/src/main/java/com/min/mj/ctrl/MemberController.java
@@ -1,6 +1,11 @@
 package com.min.mj.ctrl;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,17 +22,31 @@ import com.min.mj.model.member.IMj_Member_Dao;
 public class MemberController {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	private IMj_Member_Dao member;
+
 	/**
 	 * UserList(관리자)
 	 */
-	 @RequestMapping(value="/loginaddmin.do",method = RequestMethod.GET)
-	   public String infoBoard(Model model) {
-	      System.out.println(member.info());
-	      List<MJ_MemberDTO> memlists = member.info();
-	      model.addAttribute("memlists",memlists);
-	      return "loginaddmin";
-	   }
+	@RequestMapping(value = "/loginaddmin.do", method = RequestMethod.GET)
+	public String infoBoard(Model model) {
+		System.out.println(member.info());
+		List<MJ_MemberDTO> memlists = member.info();
+		model.addAttribute("memlists", memlists);
+		return "loginaddmin";
+	}
+
+	@RequestMapping(value = "/del.do", method = RequestMethod.GET)
+	public String del(HttpSession session, String[] seq, Model model) {
+		log.info("Welcome del.do:\t {}", Arrays.toString(seq));
+		MJ_MemberDTO mDto = (MJ_MemberDTO) member.info();
+		System.out.println(mDto);
+
+		mDto.getId();
+		model.addAttribute("mDto", mDto);
+		session.setAttribute("mem", mDto);
+		boolean isc = false;
+		return "del";
+	}
 }
